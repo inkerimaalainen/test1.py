177ca13eb67c1903d601107ac933d4e02d276dca
inkerimaalainen <ybrilka@gmail.com>
2026-02-17 15:08:33 +0300

    Update task_2-6_3.py

--- projects_2/task_2_6/task_2-6_3.py
+++ projects_2/task_2_6/task_2-6_3.py
@@ -1,8 +1,28 @@
-d = int(input('Введите группу крови донора: '))
-a = int(input('Введите группу крови пациента: '))
-if a == d or d == 0:
-    print('Переливание крови возможно')
-elif a != d and d != 0:
-    print('Переливание крови невозможно')
+phenotype_d = input("Введите фенотип группы крови донора(I, II, III, IV): ").strip().upper()
+if phenotype_d == "I":
+    print("Группа крови: 0 (I)")
+elif phenotype_d == "II":
+    print("Группа крови: A (II)")
+elif phenotype_d == "III":
+    print("Группа крови: B (III)")
+elif phenotype_d == "IV":
+    print("Группа крови: AB (IV)")
 else:
-    print('Вы некорректно указали группу крови донора или пациента')
+    print("Такой группы крови не существует")
+
+phenotype_a = input("Введите фенотип группы крови пациента(I, II, III, IV): ").strip().upper()
+if phenotype_a == "I":
+    print("Группа крови: 0 (I)")
+elif phenotype_a == "II":
+    print("Группа крови: A (II)")
+elif phenotype_a == "III":
+    print("Группа крови: B (III)")
+elif phenotype_a == "IV":
+    print("Группа крови: AB (IV)")
+else:
+    print("Такой группы крови не существует")
+
+if phenotype_d == phenotype_a or phenotype_d == 'I':
+    print('Переливание крови возможно')
+else:
+    print('Переливание крови невозможно')
