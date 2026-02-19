031275c6225f6f30c3ebabf7c6d3e83b451dc106
inkerimaalainen <ybrilka@gmail.com>
2026-02-19 14:22:37 +0300

    Add files via upload

---
+++ projects_2/task_2_7/task_2-7_1.py
@@ -0,0 +1,7 @@
+files = ["seq1", "seq2", "seq3", "seq4"]
+
+for name in files:
+
+   new_name = name + ".fasta \t Дата взятия: 19.02.2026"
+
+   print(f"{new_name}")
---
+++ projects_2/task_2_7/task_2-7_3.py
@@ -0,0 +1,7 @@
+seq = [ "ATATACGCGTA", "CTTCGGNGGA" ]
+a = str(seq[0]) + str(seq[1])
+print(a)
+for i in a:
+    print(i)
+
+print('Цикл выполнен')
