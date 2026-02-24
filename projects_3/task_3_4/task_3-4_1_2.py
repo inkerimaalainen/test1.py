9f58638d33ac5f6143f2e672e3c9013e93233740
inkerimaalainen <ybrilka@gmail.com>
2026-02-24 14:43:44 +0300

    Add files via upload

---
+++ projects_3/task_3_4/task_3-4_1_2.py
@@ -0,0 +1,5 @@
+total 12K
+4.0K drwxr-xr-x 2 inkerin inkerin 4.0K Feb 24 14:38 .
+   0 -rw-r--r-- 1 inkerin inkerin    0 Feb 24 14:38 task_3-4_1_2.py
+4.0K -rw-r--r-- 1 inkerin inkerin  177 Feb 24 14:36 task_3-4_1_1.py
+4.0K drwxr-x--- 4 inkerin inkerin 4.0K Feb 24 14:35 ..
